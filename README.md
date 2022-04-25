9e1017ab0e60c0c3055cbe1b8252ef2803c64a71
SebastianUniv <SebastianUniv@users.noreply.github.com>
2022-04-25 21:08:20 +0200

    initialise repositry

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # physicalAI
+
+## Setup
+
+To install all needed dependencies for this project run
+```sh
+pipenv install
+```
+
+In order to execute commands run the following
+```sh
+pipenv shell
+```
+
+## Usage
+
+To install new project dependencies use
+```sh
+pipenv install <package-name>
+```
---
+++ app.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import serial
+import os
+import gym
+
+# load environment variables into program
+load_dotenv()
+# get the port the arduino is running on
+port = os.getenv("PORT")
+baudrate = os.getenv("BAUDRATE")
+# setup serial connection with arduino
+arduino = serial.Serial(port=port, baudrate=baudrate, timeout=.1)
+
+# setup simulation environment
+env = gym.make('CartPole-v0')
+env.reset()
+
+
+def read():
+    ##arduino.write(bytes(x, 'utf-8'))
+    data = arduino.readline().strip()
+    return data
+
+
+while True:
+    for _ in range(1000):
+        env.render()
+        env.step(env.action_space.sample())  # take a random action
+    env.close()
+    #value = read()
+    # print(value)
