a29989f39fec8813829c0fcc47745eebd35fb029
SebastianUniv <SebastianUniv@users.noreply.github.com>
2022-05-02 19:42:59 +0200

    changed simulation

--- components/simulation.py
+++ components/simulation.py
@@ -1,146 +1,188 @@
-__credits__ = ["Carlos Luis"]
-
-from os import path
-from typing import Optional
+"""
+Classic cart-pole system implemented by Rich Sutton et al.
+Copied from http://incompleteideas.net/sutton/book/code/pole.c
+permalink: https://perma.cc/C9ZM-652R
+"""
+import math
+from typing import Optional, Union
 
 import numpy as np
 
 import gym
-from gym import spaces
+from gym import logger, spaces
 from gym.error import DependencyNotInstalled
 
 
-class PendulumEnv(gym.Env):
+class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
     """
-       ### Description
+    ### Description
 
-    The inverted pendulum swingup problem is based on the classic problem in control theory. The system consists of a pendulum attached at one end to a fixed point, and the other end being free. The pendulum starts in a random position and the goal is to apply torque on the free end to swing it into an upright position, with its center of gravity right above the fixed point.
-
-    The diagram below specifies the coordinate system used for the implementation of the pendulum's
-    dynamic equations.
-
-    ![Pendulum Coordinate System](./diagrams/pendulum.png)
-
-    -  `x-y`: cartesian coordinates of the pendulum's end in meters.
-    - `theta` : angle in radians.
-    - `tau`: torque in `N m`. Defined as positive _counter-clockwise_.
+    This environment corresponds to the version of the cart-pole problem
+    described by Barto, Sutton, and Anderson in ["Neuronlike Adaptive Elements That Can Solve Difficult Learning Control Problem"](https://ieeexplore.ieee.org/document/6313077).
+    A pole is attached by an un-actuated joint to a cart, which moves along a
+    frictionless track. The pendulum is placed upright on the cart and the goal is to balance the pole by applying forces in the left and right direction on the cart.
 
     ### Action Space
 
-    The action is a `ndarray` with shape `(1,)` representing the torque applied to free end of the pendulum.
+    The action is a `ndarray` with shape `(1,)` which can take values `{0, 1}` indicating the direction of the fixed force the cart is pushed with.
 
-    | Num | Action | Min  | Max |
-    |-----|--------|------|-----|
-    | 0   | Torque | -2.0 | 2.0 |
+    | Num | Action                 |
+    |-----|------------------------|
+    | 0   | Push cart to the left  |
+    | 1   | Push cart to the right |
 
+    **Note**: The velocity that is reduced or increased by the applied force is not fixed and it depends on the angle the pole is pointing. The center of gravity of the pole varies the amount of energy needed to move the cart underneath it
 
     ### Observation Space
 
-    The observation is a `ndarray` with shape `(3,)` representing the x-y coordinates of the pendulum's free end and its angular velocity.
+    The observation is a `ndarray` with shape `(4,)` with the values corresponding to the following positions and velocities:
 
-    | Num | Observation      | Min  | Max |
-    |-----|------------------|------|-----|
-    | 0   | x = cos(theta)   | -1.0 | 1.0 |
-    | 1   | y = sin(angle)   | -1.0 | 1.0 |
-    | 2   | Angular Velocity | -8.0 | 8.0 |
+    | Num | Observation           | Min                  | Max                |
+    |-----|-----------------------|----------------------|--------------------|
+    | 0   | Cart Position         | -4.8                 | 4.8                |
+    | 1   | Cart Velocity         | -Inf                 | Inf                |
+    | 2   | Pole Angle            | ~ -0.418 rad (-24°)  | ~ 0.418 rad (24°)  |
+    | 3   | Pole Angular Velocity | -Inf                 | Inf                |
+
+    **Note:** While the ranges above denote the possible values for observation space of each element, it is not reflective of the allowed values of the state space in an unterminated episode. Particularly:
+    -  The cart x-position (index 0) can be take values between `(-4.8, 4.8)`, but the episode terminates if the cart leaves the `(-2.4, 2.4)` range.
+    -  The pole angle can be observed between  `(-.418, .418)` radians (or **±24°**), but the episode terminates if the pole angle is not in the range `(-.2095, .2095)` (or **±12°**)
 
     ### Rewards
 
-    The reward function is defined as:
-
-    *r = -(theta<sup>2</sup> + 0.1 * theta_dt<sup>2</sup> + 0.001 * torque<sup>2</sup>)*
-
-    where `$\theta$` is the pendulum's angle normalized between *[-pi, pi]* (with 0 being in the upright position).
-    Based on the above equation, the minimum reward that can be obtained is *-(pi<sup>2</sup> + 0.1 * 8<sup>2</sup> + 0.001 * 2<sup>2</sup>) = -16.2736044*, while the maximum reward is zero (pendulum is
-    upright with zero velocity and no torque applied).
+    Since the goal is to keep the pole upright for as long as possible, a reward of `+1` for every step taken, including the termination step, is allotted. The threshold for rewards is 475 for v1.
 
     ### Starting State
 
-    The starting state is a random angle in *[-pi, pi]* and a random angular velocity in *[-1,1]*.
+    All observations are assigned a uniformly random value in `(-0.05, 0.05)`
 
     ### Episode Termination
 
-    The episode terminates at 200 time steps.
+    The episode terminates if any one of the following occurs:
+    1. Pole Angle is greater than ±12°
+    2. Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
+    3. Episode length is greater than 500 (200 for v0)
 
     ### Arguments
 
-    - `g`: acceleration of gravity measured in *(m s<sup>-2</sup>)* used to calculate the pendulum dynamics. The default value is g = 10.0 .
-
     ```
-    gym.make('Pendulum-v1', g=9.81)
+    gym.make('CartPole-v1')
     ```
 
-    ### Version History
-
-    * v1: Simplify the math equations, no difference in behavior.
-    * v0: Initial versions release (1.0.0)
-
+    No additional arguments are currently supported.
     """
 
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 30}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 50}
 
-    def __init__(self, g=10.0):
-        self.max_speed = 8
-        self.max_torque = 2.0
-        self.dt = 0.05
-        self.g = g
-        self.m = 1.0
-        self.l = 1.0
+    def __init__(self):
+        self.gravity = 9.8
+        self.masscart = 1.0
+        self.masspole = 0.1
+        self.total_mass = self.masspole + self.masscart
+        self.length = 0.5  # actually half the pole's length
+        self.polemass_length = self.masspole * self.length
+        self.force_mag = 10.0
+        self.tau = 0.02  # seconds between state updates
+        self.kinematics_integrator = "euler"
+
+        # Angle at which to fail the episode
+        self.theta_threshold_radians = 12 * 2 * math.pi / 360
+        self.x_threshold = 2.4
+
+        # Angle limit set to 2 * theta_threshold_radians so failing observation
+        # is still within bounds.
+        high = np.array(
+            [
+                self.x_threshold * 2,
+                np.finfo(np.float32).max,
+                self.theta_threshold_radians * 2,
+                np.finfo(np.float32).max,
+            ],
+            dtype=np.float32,
+        )
+
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+
         self.screen = None
         self.clock = None
         self.isopen = True
+        self.state = None
 
-        self.screen_dim = 500
+        self.steps_beyond_done = None
 
-        high = np.array([1.0, 1.0, self.max_speed], dtype=np.float32)
-        # This will throw a warning in tests/envs/test_envs in utils/env_checker.py as the space is not symmetric
-        #   or normalised as max_torque == 2 by default. Ignoring the issue here as the default settings are too old
-        #   to update to follow the openai gym api
-        self.action_space = spaces.Box(
-            low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32
+    def step(self, action):
+        err_msg = f"{action!r} ({type(action)}) invalid"
+        assert self.action_space.contains(action), err_msg
+        assert self.state is not None, "Call reset before using step method."
+        x, x_dot, theta, theta_dot = self.state
+        force = self.force_mag if action == 1 else -self.force_mag
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
+
+        # For the interested reader:
+        # https://coneural.org/florian/papers/05_cart_pole.pdf
+        temp = (
+            force + self.polemass_length * theta_dot**2 * sintheta
+        ) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta * temp) / (
+            self.length * (4.0 / 3.0 - self.masspole * costheta**2 / self.total_mass)
         )
-        self.observation_space = spaces.Box(
-            low=-high, high=high, dtype=np.float32)
+        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
 
-    def step(self, u):
-        th, thdot = self.state  # th := theta
+        if self.kinematics_integrator == "euler":
+            x = x + self.tau * x_dot
+            x_dot = x_dot + self.tau * xacc
+            theta = theta + self.tau * theta_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+        else:  # semi-implicit euler
+            x_dot = x_dot + self.tau * xacc
+            x = x + self.tau * x_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+            theta = theta + self.tau * theta_dot
 
-        g = self.g
-        m = self.m
-        l = self.l
-        dt = self.dt
+        self.state = (x, x_dot, theta, theta_dot)
 
-        u = np.clip(u, -self.max_torque, self.max_torque)[0]
-        self.last_u = u  # for rendering
-        costs = angle_normalize(th) ** 2 + 0.1 * thdot**2 + 0.001 * (u**2)
+        done = bool(
+            x < -self.x_threshold
+            or x > self.x_threshold
+            or theta < -self.theta_threshold_radians
+            or theta > self.theta_threshold_radians
+        )
 
-        newthdot = thdot + (3 * g / (2 * l) * np.sin(th) +
-                            3.0 / (m * l**2) * u) * dt
-        newthdot = np.clip(newthdot, -self.max_speed, self.max_speed)
-        newth = th + newthdot * dt
+        if not done:
+            reward = 1.0
+        elif self.steps_beyond_done is None:
+            # Pole just fell!
+            self.steps_beyond_done = 0
+            reward = 1.0
+        else:
+            if self.steps_beyond_done == 0:
+                logger.warn(
+                    "You are calling 'step()' even though this "
+                    "environment has already returned done = True. You "
+                    "should always call 'reset()' once you receive 'done = "
+                    "True' -- any further steps are undefined behavior."
+                )
+            self.steps_beyond_done += 1
+            reward = 0.0
 
-        self.state = np.array([newth, newthdot])
-        return self._get_obs(), -costs, False, {}
+        return np.array(self.state, dtype=np.float32), reward, done, {}
 
     def reset(
         self,
         *,
         seed: Optional[int] = None,
         return_info: bool = False,
-        options: Optional[dict] = None
+        options: Optional[dict] = None,
     ):
         super().reset(seed=seed)
-        high = np.array([np.pi, 1])
-        self.state = self.np_random.uniform(low=-high, high=high)
-        self.last_u = None
+        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
+        self.steps_beyond_done = None
         if not return_info:
-            return self._get_obs()
+            return np.array(self.state, dtype=np.float32)
         else:
-            return self._get_obs(), {}
-
-    def _get_obs(self):
-        theta, thetadot = self.state
-        return np.array([np.cos(theta), np.sin(theta), thetadot], dtype=np.float32)
+            return np.array(self.state, dtype=np.float32), {}
 
     def render(self, mode="human"):
         try:
@@ -151,74 +193,71 @@ class PendulumEnv(gym.Env):
                 "pygame is not installed, run `pip install gym[classic_control]`"
             )
 
+        screen_width = 600
+        screen_height = 400
+
+        world_width = self.x_threshold * 2
+        scale = screen_width / world_width
+        polewidth = 10.0
+        polelen = scale * (2 * self.length)
+        cartwidth = 50.0
+        cartheight = 30.0
+
+        if self.state is None:
+            return None
+
+        x = self.state
+
         if self.screen is None:
             pygame.init()
             pygame.display.init()
-            self.screen = pygame.display.set_mode(
-                (self.screen_dim, self.screen_dim))
+            self.screen = pygame.display.set_mode((screen_width, screen_height))
         if self.clock is None:
             self.clock = pygame.time.Clock()
 
-        self.surf = pygame.Surface((self.screen_dim, self.screen_dim))
+        self.surf = pygame.Surface((screen_width, screen_height))
         self.surf.fill((255, 255, 255))
 
-        bound = 2.2
-        scale = self.screen_dim / (bound * 2)
-        offset = self.screen_dim // 2
+        l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+        axleoffset = cartheight / 4.0
+        cartx = x[0] * scale + screen_width / 2.0  # MIDDLE OF CART
+        carty = 100  # TOP OF CART
+        cart_coords = [(l, b), (l, t), (r, t), (r, b)]
+        cart_coords = [(c[0] + cartx, c[1] + carty) for c in cart_coords]
+        gfxdraw.aapolygon(self.surf, cart_coords, (0, 0, 0))
+        gfxdraw.filled_polygon(self.surf, cart_coords, (0, 0, 0))
 
-        rod_length = 1 * scale
-        rod_width = 0.2 * scale
-        l, r, t, b = 0, rod_length, rod_width / 2, -rod_width / 2
-        coords = [(l, b), (l, t), (r, t), (r, b)]
-        transformed_coords = []
-        for c in coords:
-            c = pygame.math.Vector2(c).rotate_rad(self.state[0] + np.pi / 2)
-            c = (c[0] + offset, c[1] + offset)
-            transformed_coords.append(c)
-        gfxdraw.aapolygon(self.surf, transformed_coords, (204, 77, 77))
-        gfxdraw.filled_polygon(self.surf, transformed_coords, (204, 77, 77))
-
-        gfxdraw.aacircle(self.surf, offset, offset,
-                         int(rod_width / 2), (204, 77, 77))
-        gfxdraw.filled_circle(
-            self.surf, offset, offset, int(rod_width / 2), (204, 77, 77)
+        l, r, t, b = (
+            -polewidth / 2,
+            polewidth / 2,
+            polelen - polewidth / 2,
+            -polewidth / 2,
         )
 
-        rod_end = (rod_length, 0)
-        rod_end = pygame.math.Vector2(
-            rod_end).rotate_rad(self.state[0] + np.pi / 2)
-        rod_end = (int(rod_end[0] + offset), int(rod_end[1] + offset))
+        pole_coords = []
+        for coord in [(l, b), (l, t), (r, t), (r, b)]:
+            coord = pygame.math.Vector2(coord).rotate_rad(-x[2])
+            coord = (coord[0] + cartx, coord[1] + carty + axleoffset)
+            pole_coords.append(coord)
+        gfxdraw.aapolygon(self.surf, pole_coords, (202, 152, 101))
+        gfxdraw.filled_polygon(self.surf, pole_coords, (202, 152, 101))
+
         gfxdraw.aacircle(
-            self.surf, rod_end[0], rod_end[1], int(
-                rod_width / 2), (204, 77, 77)
+            self.surf,
+            int(cartx),
+            int(carty + axleoffset),
+            int(polewidth / 2),
+            (129, 132, 203),
         )
         gfxdraw.filled_circle(
-            self.surf, rod_end[0], rod_end[1], int(
-                rod_width / 2), (204, 77, 77)
+            self.surf,
+            int(cartx),
+            int(carty + axleoffset),
+            int(polewidth / 2),
+            (129, 132, 203),
         )
 
-        fname = path.join(path.dirname(__file__), "assets/clockwise.png")
-        img = pygame.image.load(fname)
-        if self.last_u is not None:
-            scale_img = pygame.transform.smoothscale(
-                img, (scale * np.abs(self.last_u) / 2,
-                      scale * np.abs(self.last_u) / 2)
-            )
-            is_flip = bool(self.last_u > 0)
-            scale_img = pygame.transform.flip(scale_img, is_flip, True)
-            self.surf.blit(
-                scale_img,
-                (
-                    offset - scale_img.get_rect().centerx,
-                    offset - scale_img.get_rect().centery,
-                ),
-            )
-
-        # drawing axle
-        gfxdraw.aacircle(self.surf, offset, offset,
-                         int(0.05 * scale), (0, 0, 0))
-        gfxdraw.filled_circle(self.surf, offset, offset,
-                              int(0.05 * scale), (0, 0, 0))
+        gfxdraw.hline(self.surf, 0, screen_width, carty, (0, 0, 0))
 
         self.surf = pygame.transform.flip(self.surf, False, True)
         self.screen.blit(self.surf, (0, 0))
@@ -241,7 +280,3 @@ class PendulumEnv(gym.Env):
             pygame.display.quit()
             pygame.quit()
             self.isopen = False
-
-
-def angle_normalize(x):
-    return ((x + np.pi) % (2 * np.pi)) - np.pi
